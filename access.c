2f9767540a873c6157ab3d02d7dd0be2b4328cf9
Xi Han <xihan94@ucla.edu>
2014-03-14 09:09:47 -0700

    Updated access.c

--- access.c
+++ access.c
@@ -1,7 +1,13 @@
 #include "access.h"
 
 // Tentative data structures:
-// A dictionary data structure. Key = file name, Value = a pointer to a struct of both a white list and a black list of ip addresses.
+// A list data structure of perm_struct.
+
+// struct perm_struct {
+//     char *file_name;
+//     char **black_list;
+//     char **white_list;	
+// };
 
 // RULES:
 // 1. if an IP address is explicitly declared in the black list (e.g. 192.168.1.103), block.
@@ -10,7 +16,6 @@
 //     2.2 if the IP address is declared in the white list, grant.
 // 3. if an IP address is not declared or included in the black list, grant.
 
-// glib hash table and list may help.
 
 
 void access_control_setup(const char *ac_filename) {
@@ -25,8 +30,8 @@ void access_control_setup(const char *ac_filename) {
 
 int access_control_verify(const char *ip_addr, const char *filename) {
 
-	// Use filename as key to search the dictionary.
-	// If file is in the dictionary, then search the lists for that key.
+	// Search the list for the file name.
+	// If file is in the list, then search the lists for that file.
 		// If ip_addr is indicated in the list as blocked (according to apache semantics), return 0 (access denied).
 	// else return 1 (access allowed).
 
@@ -34,21 +39,21 @@ int access_control_verify(const char *ip_addr, const char *filename) {
 
 void access_control_grant_permission(const char *ip_addr, const char *filename) {
 
-	// Use filename as key to search the dictionary.
-	// If file is in the dictionary, then search the lists for that key.
+	// Search the list for the file name.
+	// If file is in the list, then search the lists for that file.
 		// If ip_addr is indicated in the list as blocked, modify the struct to grant permission to that ip address.
-			// If nothing left in the black list, detele the entry from the dictionary.
+			// If nothing left in the black list, detele the entry from the the permission struct list.
 		// Else simply return (nothing changed).
 	// Else simply return. 
 }
 
 void access_control_revoke_permission(const char *ip_addr, const char *filename) {
 
-	// Use filename as key to search the dictionary.
-	// If file is in the dictionary, then search the lists for that key.
+	// Search the list for the file name.
+	// If file is in the list, then search the lists for that file.
 		// If ip_addr is indicated in the list as blocked, do nothing.
 		// Else change the data structure to revoke the permission.
-	// Else create a new entry in the dictionary, and add ip_addr to the black list. 
+	// Else create a new entry in the permission struct list, and add ip_addr to the black list. 
 
 }
 
